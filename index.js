c1512063ef11d64e56439424fc76f55d89ba7450
Cameron Brantley <brantley.cameron@gmail.com>
2020-02-07 20:56:45 -0600

    practice JS

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
-// create a variable called "name" that references a string
-const name = "Cameron";
-// create a variable called "favoriteSong" that references a string
-let favoriteSong = "Sweet Home Alabama";
-// create a variable called "wage" that references a number
-let wage = 12;
-// create a variable called "age" that references a number
-let age = 34;
-// create a variable called "onlyChild" that references a boolean
-const onlyChild = false;
-// create a variable called "satisfied" that references a boolean
-let satisfied = false;
-// create a variable called "favoriteThing" that references ANY primitive value
-let favoriteThing = "car";
-// print the data type of "favoriteThing"
-console.log(typeof favoriteThing);
-// You heard a new song that became your favorite.
-// Reassign the variable "favoriteSong"
-favoriteSong = "Cowboy";
-// You no longer like the type of data stored in "favoriteThing"
-// Reassign "favoriteThing" to a different primitive data type and print its type
-favoriteThing = 14;
-console.log(typeof favoriteThing);
-// Happy Birthday!
-// Reassign "age" to a new value
-age = 35;
-// You got a big raise at work.
-// Reassign "wage" to a new value
-wage = 45;
-// Your raise changed your job satisfaction.
-// Assign "satisfied" to true
-satisfied = true;
-// use "console.log()" and "+" to print a sentence that includes two variables
-console.log("I am " + age + " and my favorite song is " + favoriteSong);
+const me = {};
+
+me.name = "Cameron";
+me.age = 34;
+me.occupation = "teacher";
+
+console.log(me);
+
+const sister = {
+  name: "Melissa",
+  age: 34,
+  occupation: "nurse"
+};
+console.log(sister);
+
+const bigSister = {
+  name: "Alyson",
+  age: 36,
+  occupation: "Mom"
+};
+console.log(bigSister);
+
+const family = {
+  name: "Brantley",
+  number: 5,
+  pets: {
+    cat: "Charlie",
+    dog: "Rudy"
+  }
+};
+console.log(family.pets.cat);
