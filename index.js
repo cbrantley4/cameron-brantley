2b3f6be1f1c13d784ed4689c6f7dd45b606d3111
Cameron Brantley <brantley.cameron@gmail.com>
2020-02-07 21:29:05 -0600

    Practice symbols

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
-const me = {};
+const x = 6;
+const y = 3;
 
-me.name = "Cameron";
-me.age = 34;
-me.occupation = "teacher";
-
-console.log(me);
-
-const sister = {
-  name: "Melissa",
-  age: 34,
-  occupation: "nurse"
-};
-console.log(sister);
-
-const bigSister = {
-  name: "Alyson",
-  age: 36,
-  occupation: "Mom"
-};
-console.log(bigSister);
-
-const family = {
-  name: "Brantley",
-  number: 5,
-  pets: {
-    cat: "Charlie",
-    dog: "Rudy"
-  }
-};
-console.log(family.pets.cat);
+console.log(x < y);
+console.log(x <= y);
+console.log(x == y);
